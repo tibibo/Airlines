fa8049bb840ed7e245738f799e26175dcbbbbb7c
Vladimir Drapalyuk <dvs@Vladimirs-MacBook-Air.local>
2017-05-01 17:14:27 +0200

    Initial Commit. Created project in xcode.

---
+++ Airlines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airlines
+//
+//  Created by Vladimir Drapalyuk on 01/05/2017.
+//  Copyright © 2017 Tibibo, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AirlinesTests/AirlinesTests.swift
@@ -0,0 +1,36 @@
+//
+//  AirlinesTests.swift
+//  AirlinesTests
+//
+//  Created by Vladimir Drapalyuk on 01/05/2017.
+//  Copyright © 2017 Tibibo, s.r.o. All rights reserved.
+//
+
+import XCTest
+@testable import Airlines
+
+class AirlinesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
